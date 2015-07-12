758fa53682218f1a1de67fe8b51e8323e6ed8907
Preethish <Preethish@Vitthal-PC>
2015-07-12 19:24:16 +0530

    Recipe DAO

---
+++ src/main/java/com/ci/data/dao/RecipeDao.java
@@ -0,0 +1,7 @@
+package com.ci.data.dao;
+
+import com.ci.data.Recipe;
+
+public interface RecipeDao extends CookIndyaDao<Recipe>{
+
+}
---
+++ src/main/java/com/ci/data/dao/RecipeDaoImpl.java
@@ -0,0 +1,12 @@
+package com.ci.data.dao;
+
+import com.ci.data.Recipe;
+
+public class RecipeDaoImpl extends CookIndyaDaoImpl<Recipe> implements RecipeDao{
+
+	public RecipeDaoImpl(Class<Recipe> clasInput) {
+		super(clasInput);
+		// TODO Auto-generated constructor stub
+	}
+
+}
