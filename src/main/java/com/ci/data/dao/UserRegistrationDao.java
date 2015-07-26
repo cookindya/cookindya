bf407d6f1d5dfa3e0445c489de8e07ce45343f99
Uday <Uday@192.168.254.100>
2015-07-26 12:48:35 +0530

    changes related to registration

---
+++ src/main/java/com/ci/data/dao/UserRegistrationDao.java
@@ -0,0 +1,7 @@
+package com.ci.data.dao;
+
+import com.ci.data.UserRegistration;
+
+public interface UserRegistrationDao extends  CookIndyaDao<UserRegistration> {
+
+}
---
+++ src/main/java/com/ci/data/dao/UserRegistrationDaoImpl.java
@@ -0,0 +1,11 @@
+package com.ci.data.dao;
+
+import com.ci.data.UserRegistration;
+
+public class UserRegistrationDaoImpl  extends CookIndyaDaoImpl<UserRegistration> implements UserRegistrationDao{
+
+	public UserRegistrationDaoImpl(Class<UserRegistration> clasInput) {
+		super(clasInput);
+		// TODO Auto-generated constructor stub
+	}
+}
